6920867a2b618e434139caf93d144ef87cf686af
pallavi1147 <127471159+pallavi1147@users.noreply.github.com>
2023-03-15 01:03:42 -0700

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,13 @@
+class Arithmetic{
+public static void main(String args[]){
+System.out.println("The two number is :");
+int l =5;
+int b =6;
+System.out.println(l + " " + b);
+int Area = l*b;
+int Perimeter =2*(l+b);
+System.out.println("The Area of rectangle is = " + Area);
+System.out.println("The Perimeter of rectangle is = " + Perimeter);
+
+}
+}
