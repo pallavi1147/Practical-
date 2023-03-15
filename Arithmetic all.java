708e42ca6f430d0a35a7b7ec2c8e1fc7c18f4dc9
pallavi1147 <127471159+pallavi1147@users.noreply.github.com>
2023-03-15 09:34:33 -0700

    Add files via upload

---
+++ Arithmetic all.java
@@ -0,0 +1,17 @@
+class All{
+public static void main(String []args){
+System.out.println("The two number is :");
+int a =5;
+int b =6;
+System.out.println(a + " " + b);
+int sum = a+b;
+int difference = a-b;
+int mul = a*b;
+int div =a/b;
+System.out.println("The sum is = " + sum);
+System.out.println("The difference is = " + difference);
+System.out.println("The mul is = " + mul);
+System.out.println("The div is = " + div);
+
+}
+}
