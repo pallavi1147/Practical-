deafffe405dbb4685d9c72057d63ed3bc306fe9e
pallavi1147 <127471159+pallavi1147@users.noreply.github.com>
2023-03-16 09:39:44 -0700

    Add files via upload

---
+++ Logical Operator.java
@@ -0,0 +1,10 @@
+class Logical Operator{
+public static void main(String args[]){
+int a=10;
+int b=5;
+int c=20;
+System.out.println(a>b && a<c);
+System.out.println(a=b || b>c);
+System.out.println(a>c && b<c);
+System.out.println(a<c || b>a);
+}}
