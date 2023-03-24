160c2c34be783a928b959038f0e79c0bb96ed958
pallavi1147 <127471159+pallavi1147@users.noreply.github.com>
2023-03-23 21:24:58 -0700

    Add files via upload

---
+++ ForLoop3.java
@@ -0,0 +1,10 @@
+class ForLoop3{
+public static void main(String args[]){
+int i;
+System.out.println("The first ten numbers are:\n");
+for(i=1; i<=10; i++){
+System.out.println(i);
+}
+System.out.println("\n");
+}
+}
