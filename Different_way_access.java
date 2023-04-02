8d7d6b779d2f2b0afa4e41666dd1830457bd5720
pallavi1147 <127471159+pallavi1147@users.noreply.github.com>
2023-04-02 21:47:10 +0530

    Add files via upload

---
+++ Different_way_access.java
@@ -0,0 +1,10 @@
+package package_eg;
+
+import com.mypackage;
+
+public class Different_way_access {
+ public static void main(String[] args) {
+	 mypackage obj= new mypackage();
+	 obj.display();
+ }
+}
