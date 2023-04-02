a780e79309e26919e24a933ea7a4b04d3d962e65
pallavi1147 <127471159+pallavi1147@users.noreply.github.com>
2023-04-02 21:41:35 +0530

    Add files via upload

---
+++ Protected_AM.java
@@ -0,0 +1,10 @@
+
+package access_modifier;
+import package_eg.Protected_Test;
+//if you want to access protected method, it is possible only through inheritance
+ class Protected_AM extends Protected_Test {
+	public static void main(String[] args) {
+		Protected_AM ob = new Protected-Am();
+		ob.result();
+	}
+}
