853c6c179689d65d311442f2f5f701904c0c68b7
pallavi1147 <127471159+pallavi1147@users.noreply.github.com>
2023-04-02 21:42:54 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,12 @@
+package example;
+
+import core.Simple_test;
+
+public class test {
+
+		public static void main(String[] args) {
+			 Simple_test obj= new Simple_test();
+			 obj.display();
+		 }
+	}
+
