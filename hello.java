098c26b4d0f4ba91b1034a5c88af8275fe9dca27
pallavi1147 <127471159+pallavi1147@users.noreply.github.com>
2023-04-02 21:43:34 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,10 @@
+package packageeg;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Creating our first package");
+	}
+
+}
